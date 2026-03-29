f15fd2a7900cecb6cfc9800698fd17fdfd498510
zaoo-star <zaoo@post.cz>
2026-03-29 07:46:15 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,24 @@
+const CACHE = 'bucovice-v2';
+const ASSETS = ['./'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  if (e.request.mode === 'navigate') {
+    e.respondWith(
+      fetch(e.request).catch(() => caches.match('./'))
+    );
+  }
+});
